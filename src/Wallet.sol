c10e62f1d3b63de2c22e0c6cb96daefb267161b5
Daksh <akshd@outlook.com>
2023-10-29 21:27:30 +0530

    feat: add tokenhandler

--- src/Wallet.sol
+++ src/Wallet.sol
@@ -4,13 +4,20 @@ pragma solidity ^0.8.19;
 
 import {BaseAccount} from "account-abstraction/core/BaseAccount.sol";
 import {UserOperation} from "account-abstraction/interfaces/UserOperation.sol";
-import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 import {IEntryPoint} from "account-abstraction/interfaces/IEntryPoint.sol";
 
+import {TokenCallbackHandler} from "./TokenCallbackHandler.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 import {Initializable} from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
 
-contract Wallet is BaseAccount, Initializable {
+contract Wallet is
+    BaseAccount,
+    Initializable,
+    UUPSUpgradeable,
+    TokenCallbackHandler
+{
     using ECDSA for bytes32;
     using MessageHashUtils for bytes32;
 
@@ -20,10 +27,18 @@ contract Wallet is BaseAccount, Initializable {
 
     event WalletInitialized(IEntryPoint indexed entryPoint, address[] owners);
 
+    modifier _requireFromEntryPointOrFactory() {
+        if (msg.sender != address(_entryPoint) || msg.sender != walletFactory)
+            revert();
+        _;
+    }
+
     constructor(IEntryPoint __entryPoint, address factory) {
         _entryPoint = __entryPoint;
         walletFactory = factory;
     }
+
+    // Initialization
 
     function initialize(address[] memory initialOwners) public initializer {
         _initialize(initialOwners);
@@ -37,6 +52,49 @@ contract Wallet is BaseAccount, Initializable {
 
     function entryPoint() public view override returns (IEntryPoint) {
         return _entryPoint;
+    }
+
+    // Main Function
+
+    function execute(
+        address dest,
+        uint256 value,
+        bytes calldata fn
+    ) external _requireFromEntryPointOrFactory {
+        _call(dest, value, fn);
+    }
+
+    function executeBatch(
+        address[] calldata dests,
+        uint256[] calldata values,
+        bytes[] calldata fns
+    ) external _requireFromEntryPointOrFactory {
+        if (dests.length != fns.length) revert();
+        if (values.length != fns.length) revert();
+        uint256 len = dests.length;
+        for (uint256 i = 0; i < len; ) {
+            _call(dests[i], values[i], fns[i]);
+            unchecked {
+                i++;
+            }
+        }
+    }
+
+    // Internals
+
+    function _call(
+        address targetAddress,
+        uint256 value,
+        bytes memory data
+    ) internal {
+        (bool success, bytes memory result) = targetAddress.call{value: value}(
+            data
+        );
+        if (!success) {
+            assembly {
+                revert(add(result, 32), mload(result))
+            }
+        }
     }
 
     function _validateSignature(
@@ -54,4 +112,26 @@ contract Wallet is BaseAccount, Initializable {
 
         return 0;
     }
+
+    function _authorizeUpgrade(
+        address
+    ) internal override _requireFromEntryPointOrFactory {}
+
+    // Helpers
+
+    function encodeSignatures(
+        bytes[] memory signatures
+    ) public pure returns (bytes memory) {
+        return abi.encode(signatures);
+    }
+
+    function getDeposit() public view returns (uint256) {
+        return entryPoint().balanceOf(address(this));
+    }
+
+    function addDeposit() public payable {
+        entryPoint().depositTo{value: msg.value}(address(this));
+    }
+
+    receive() external payable {}
 }
